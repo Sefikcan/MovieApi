c4adb20d1eefb63ace959dfca8928a6bf157408a
SefikCan <sefikcankanber@gmail.com>
2018-09-20 23:32:55 +0300

    add user register operations

--- MovieApi/routes/index.js
+++ MovieApi/routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+const bcrypt = require('bcrypt');
+
+//Models
+const User = require('../models/User');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
+
+router.post('/register', (req, res, next) => {
+  const {
+    username,
+    password
+  } = req.body;
+
+  bcrypt.hash(password, 10).then((hash) => {
+    const user = new User({
+      username:username,
+      password:hash
+    });
+
+    const promise = user.save();
+    promise.then((data) => {
+      res.json(data);
+    }).catch((err) => {
+      res.json(err);
+    });
+  });
+
+})
 
 module.exports = router;
